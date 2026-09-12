a9e580d88a0fb0495e31cd0a2450daca102dbdbd
champi95250 <benoit.afonso@gmail.com>
2026-09-12 17:02:38 +0200

    V1.5a
    Update Minimap

--- Plugins/Arcky's Region Map/000_Settings_and_Setup/001_RegionMap_LocationInfo.rb
+++ Plugins/Arcky's Region Map/000_Settings_and_Setup/001_RegionMap_LocationInfo.rb
@@ -25,14 +25,14 @@ module ARMLocationPreview
   BourgPrisme = {
     description: _INTL(
       "<ac><b>Le point de départ de toute aventure.</b><br>" \
-      "Un petit bourg paisible où se trouve le célèbre laboratoire Pokémon.</ac>"
+      "Un petit bourg paisible où se trouve le célèbre Laboratoire Pokémon.</ac>"
     )
   }
 
   Route1 = {
     description: _INTL(
-      "<ac>Une route calme reliant Bourg-Prisme à Rosaville.<br>" \
-      "Parfaite pour faire ses premiers pas… et ses premiers combats.</ac>"
+      "<ac><b>Une route calme reliant Bourg-Prisme à Rosaville.</b><br>" \
+      "Parfaite pour faire ses premiers pas… et livrer ses premiers combats.</ac>"
     )
   }
 
@@ -45,7 +45,7 @@ module ARMLocationPreview
 
   Route2 = {
     description: _INTL(
-      "<ac>Une route verdoyante bordée de fleurs sauvages.<br>" \
+      "<ac><b>Une route verdoyante bordée de fleurs sauvages.</b><br>" \
       "De jeunes Dresseurs y viennent pour faire leurs premières armes.</ac>"
     )
   }
@@ -53,41 +53,42 @@ module ARMLocationPreview
   Claircour = {
     description: _INTL(
       "<ac><b>Une ville simple, animée et accueillante.</b><br>" \
-      "Ses rues pavées attirent voyageurs, Dresseurs et garçons beaucoup trop sûrs d'eux.</ac>"
+      "Ses rues pavées attirent voyageurs, Dresseurs et garçons beaucoup trop sûrs d’eux.</ac>"
     )
   }
 
   BoisdesSerments = {
     description: _INTL(
-      "<ac><b>Une forêt paisible chargée de mystères.</b><br>" 
+      "<ac><b>Une forêt paisible chargée de mystères.</b><br>" \
+      "On raconte que les promesses prononcées sous ses arbres ne doivent jamais être brisées.</ac>"
     )
   }
 
   Route3 = {
     description: _INTL(
-      "<ac>Une longue route sauvage à l'ouest de Claircour.<br>" \
+      "<ac><b>Une longue route sauvage à l’ouest de Claircour.</b><br>" \
       "Une grotte mystérieuse se trouve à son extrémité, mais son accès semble condamné.</ac>"
     )
   }
 
   Caldéon = {
     description: _INTL(
-      "<ac>Une Ville thermale baignée de soleil<br>" \
-      "Sa chaleur attire de nombreux Pokémon et abrite l’Arène de type Feu.</ac>"
+      "<ac><b>Une ville thermale baignée de soleil.</b><br>" \
+      "Sa chaleur attire de nombreux Pokémon et elle abrite l’Arène de type Feu.</ac>"
     )
   }
 
   GrotteDélasuée = {
     description: _INTL(
-      "<ac>Une Grotte Chaude baignée d'humidité<br>" \
-      "Sa chaleur et son humidité sont un lieu unique a Prisméa.</ac>"
+      "<ac><b>Une grotte chaude et chargée d’humidité.</b><br>" \
+      "Son atmosphère étouffante en fait un lieu unique dans toute la région de Prisméa.</ac>"
     )
   }
 
   Route4 = {
     description: _INTL(
       "<ac><b>Une route verdoyante baignée de chaleur.</b><br>" \
-      "Son climat agréable en fait un lieu idéal pour se promener.</ac>"
+      "Ses fleurs, son petit lac et son climat agréable invitent les voyageurs à ralentir.</ac>"
     )
   }
 
@@ -100,17 +101,29 @@ module ARMLocationPreview
 
   Route5 = {
     description: _INTL(
-      "<ac><b>Une route menant sur vers Belécume.</b><br>" \
-      "Sa plage si agréable en fait un lieu idéal pour se baigner.</ac>"
+      "<ac><b>Une route côtière menant vers Bélécume.</b><br>" \
+      "Sa plage agréable et ses eaux claires en font un lieu idéal pour se baigner.</ac>"
     )
   }
 
-  Belécume = {
+  Bélécume = {
     description: _INTL(
-      "<ac>Une Ile Ville baignée de la mer<br>" \
-      "Sa chaleur attire de nombreux Pokémon et abrite l’Arène de type Eau.</ac>"
+      "<ac><b>Une presqu’île bercée par la mer.</b><br>" \
+      "Ses plages attirent les touristes et la ville abrite l’Arène de type Eau.</ac>"
     )
   }
 
-  
+  Route6 = {
+    description: _INTL(
+      "<ac><b>Une route très fréquentée menant à Prismapolis.</b><br>" \
+      "Dresseurs, journalistes et Pokémon urbains s’y croisent avant l’entrée dans la capitale.</ac>"
+    )
+  }
+
+  Prismapolis = {
+    description: _INTL(
+      "<ac><b>La grande métropole de Prisméa.</b><br>" \
+      "Commerces, Casino, Zone de Combat et divertissements illuminent ses rues à toute heure.</ac>"
+    )
+  }
 end
